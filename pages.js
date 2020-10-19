8ef51de9c32b4d21fb6359f50e28e7f17f0b1415
khaledhasanzami <42893663+khaledhasanzami@users.noreply.github.com>
2020-10-19 23:07:03 +0600

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/khaledhasanzami/khaledhasanzami.github.io";
+const repoURL = "https://github.com/khaledhasanzami/khaledhasanzami.github.io.git";
 
 ghpages.publish(
   pathname,
